7f847cc847d5ec0f16fd39779c6f1595f33f848b
辰韶 <841546312@qq.com>
2021-11-03 21:42:55 +0800

    添加了更好看的表格

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import mail
 import init
 import datetime
 import codecs
+from prettytable import PrettyTable
 
 if connectLine.connect() != 1:
     f = open("errorlog.log", "w")
@@ -15,15 +16,31 @@ if connectLine.connect() != 1:
 ls_1 = sea.run()
 ls_2 = rp.run()
 
-content = "|用户|应用|状态|\n"
+content = PrettyTable()
+content.field_names = ["用户", "应用", "状态"]
 for us in ls_1:
-    content += "|{}|{}|{}|\n".format(us[0], "出校申请", us[1])
-
+    if us[1] == 1:
+        content.add_row([us[0], "出校申请", "成功"])
+    elif us[0] == 0:
+        content.add_row(us[0], "出校申请", "超时")
+    else:
+        content.add_row(us[0], "出校申请", "失败")
 for us in ls_2:
-    content += "|{}|{}|{}|\n".format(us[0], "报平安", us[1])
+    if us[1] == 1:
+        content.add_row([us[0], "报平安", "成功"])
+    elif us[0] == 0:
+        content.add_row(us[0], "报平安", "超时")
+    else:
+        content.add_row(us[0], "报平安", "失败")
+# content = "|用户|应用|状态|\n"
+# for us in ls_1:
+#     content += "|{}|{}|{}|\n".format(us[0], "出校申请", us[1])
+#
+# for us in ls_2:
+#     content += "|{}|{}|{}|\n".format(us[0], "报平安", us[1])
 
 if init.mail_sw == 1:
-    mail.send_mail(content)
+    mail.send_mail(content.get_string())
 tm = datetime.datetime.now().strftime("%m-%d %H:%M:%S")
 with codecs.open("log.log", "a+", "utf-8") as f:
     f.write(tm + '\n' + content)
